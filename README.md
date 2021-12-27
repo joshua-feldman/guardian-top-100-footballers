28d760bb01d49c33d6507265f65e30d7f6396033
Joshua Feldman <joshuafeldman@Joshuas-MacBook-Pro.local>
2021-12-27 16:08:17 +0000

    commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# guardian-top-100-footballers
+# The Guardian's Top 100 Footballers
 Data scraped from the Guardian's annual ranking of the top 100 footballers
---
+++ scrape-female.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(rvest)
+
+# Save URLS
+url_2018 <- "https://interactive.guim.co.uk/docsdata/1z83ACM3aUr_toXtNlhYPh394Wn9LaGt2_M5Lifgjgrc.json"
+url_2019 <- "https://interactive.guim.co.uk/docsdata/1AH6d9-VMIGN9ya326QnbOqpW2yT-RnGJ9vSDN7sNsj8.json"
+url_2020 <- "https://interactive.guim.co.uk/docsdata/1zWOS3gGwb4sYnyL-FEKysvtgAb3E1R_KolnT0pDIbAQ.json"
+url_2021 <- "https://interactive.guim.co.uk/docsdata/1kzKAF_X_ZAZGj_DyamcMjauxblQC9NP_yODYK4iQGHA.json"
+
+# Save data to data frames
+df_2018 <- jsonlite::fromJSON(url_2018) %>% as.data.frame()
+df_2019 <- jsonlite::fromJSON(url_2019) %>% as.data.frame()
+df_2020 <- jsonlite::fromJSON(url_2020) %>% as.data.frame()
+df_2021 <- jsonlite::fromJSON(url_2021) %>% as.data.frame()
+
+# Write to CSV
+write.csv(df_2018, "data/female-raw-2018.csv", row.names=FALSE)
+write.csv(df_2019, "data/female-raw-2019.csv", row.names=FALSE)
+write.csv(df_2020, "data/female-raw-2020.csv", row.names=FALSE)
+write.csv(df_2021, "data/female-raw-2021.csv", row.names=FALSE)
+
+# Create modified dataframes
+new_colnames <- c("rank", "name", "position", "club", "nationality", "age", "league", "year")
+
+df_2018_mod <- df_2018 %>% 
+  select(ends_with(c("rank", "sheets.players.name", "position")), 
+         contains(c("club", "nationality", "age.on", "league"))) %>% 
+  mutate(year = "2018")
+
+df_2019_mod <- df_2019 %>% 
+  select(ends_with(c("rank", "sheets.players.name", "position")), 
+         contains(c("club", "nationality", "age.on", "league"))) %>% 
+  mutate(year = "2019")
+
+df_2020_mod <- df_2020 %>% 
+  select(ends_with(c("rank", "sheets.players.name", "position")), 
+         contains(c("club", "nationality", "age.on", "league"))) %>% 
+  mutate(year = "2020")
+
+df_2021_mod <- df_2021 %>% 
+  select(ends_with(c("rank", "sheets.players.name", "position")), 
+         contains(c("club", "nationality", "age.on", "league"))) %>% 
+  mutate(year = "2021")
+
+colnames(df_2018_mod) <- new_colnames
+colnames(df_2019_mod) <- new_colnames
+colnames(df_2020_mod) <- new_colnames
+colnames(df_2021_mod) <- new_colnames
+
+df_combined <- df_2018_mod %>% 
+  rbind(df_2019_mod) %>% 
+  rbind(df_2020_mod) %>% 
+  rbind(df_2021_mod) %>% 
+  mutate(rank = as.integer(rank)) %>% 
+  mutate(age = as.integer(age))
+
+df_combined <- df_combined[,c("year", "rank", "name", "position", "age", "nationality", "club", "league")]
+
+write.csv(df_combined, "data/female-clean-combined.csv", row.names=FALSE)
